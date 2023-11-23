1a5cce2297d08f2b93289383606e8f75c14c48f5
aent <alintssr@gmail.com>
2023-11-23 17:25:55 -0500

    header scaffolding

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import './Header.scss'
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <div className="header">
+                    <ul>
+                        <li>North America</li>
+                        <li>South America</li>
+                        <li>Europe</li>
+                        <li>Africa</li>
+                        <li>Asia</li>
+                        <li>Australia</li>
+                    </ul>
+                </div>
+            </header>
+
+
+        </>
+    )
+}
