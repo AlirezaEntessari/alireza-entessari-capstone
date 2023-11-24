42a49ac743b32020342160e29dc80bb1faab7461
aent <alintssr@gmail.com>
2023-11-24 03:24:56 -0500

    added responsiveness

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,32 @@
-import './Header.scss'
+import "./Header.scss";
 
 export const Header = () => {
-    return (
-        <>
-            <header>
-                <div className="header">
-                    <ul>
-                        <li>North America</li>
-                        <li>South America</li>
-                        <li>Europe</li>
-                        <li>Africa</li>
-                        <li>Asia</li>
-                        <li>Australia</li>
-                    </ul>
-                </div>
-            </header>
-
-
-        </>
-    )
-}
+  return (
+    <>
+      <header>
+        <div className="header">
+          <ul className="header__list">
+            <div className="header__list--item1">North America</div>
+            <div className="header__list--item2">South America</div>
+            <div className="header__list--item3">Europe</div>
+            <div className="header__list--item4">Africa</div>
+            <div className="header__list--item5">Asia</div>
+            <div className="header__list--item6">Australia</div>
+          </ul>
+        </div>
+        <div className="horizontal-header">
+          <nav className="horizontal-header__nav">
+            <ul className="horizontal-header__list">
+              <li className="horizontal-header__list--north-america">North America</li>
+              <li className="horizontal-header__list--south-america">South America</li>
+              <li className="horizontal-header__list--europe">Europe</li>
+              <li className="horizontal-header__list--africa">Africa</li>
+              <li className="horizontal-header__list--asia">Asia</li>
+              <li className="horizontal-header__list--australia">Australia</li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+};
